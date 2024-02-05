5e118f87fdf3a185d35e66c71e6a5c6feec51392
Vixen-1 <ayushisaxena24111999@gmail.com>
2024-02-05 16:31:25 +0530

    Added Currency configuration

--- src/main/java/effigo/ayushi/learning/SpringBootTrainingApplication.java
+++ src/main/java/effigo/ayushi/learning/SpringBootTrainingApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnSpringBootApplication {
+public class SpringBootTrainingApplication {
 	public static void main(String[] args){
-		SpringApplication.run(LearnSpringBootApplication.class, args);
+		SpringApplication.run(SpringBootTrainingApplication.class, args);
 	}
 }
